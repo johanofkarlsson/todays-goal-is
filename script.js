0981321713390cc6f41eebc46d3840331021a5cb
johanofkarlsson <40716365+johanofkarlsson@users.noreply.github.com>
2018-08-03 07:02:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let goalContent = "Tell someone what you love about them!";
+
+let quoteContent = '"If you fail to plan, you plan to fail"';
+
+function setGoal(){
+	document.getElementById("todaysGoal").innerHTML = goalContent;
+}
+
+function setQuote(){
+	document.getElementById("inspQuote").innerHTML = quoteContent;
+}
+
+setGoal();
+setQuote();
