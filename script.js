e470d0d01fadb87276c9aef8cf0108d49269168f
Johan Karlsson <40716365+johanofkarlsson@users.noreply.github.com>
2018-08-04 11:31:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let goalContent = "Tell someone what you love about them!";
 
-let quoteContent = '"If you fail to plan, you plan to fail"';
+//A list of goals that will randomize when the user refreshes the site
+let goalsList = ["Tell someone something you love about them!", 
+					"Write that text, e-mail or, better yet, make that call you've been putting off!",
+					"Take a long walk and if you can, take a picture of something beautiful"						
+];
+
+//A list of quotes that will randomize when the user refreshes the site
+let quotesList = [
+				'"If you fail to plan, you plan to fail" - Benjamin Franklin',
+				"Don't let yesterday take up too much of today",
+				"The best way to prepare for tomorrow is to do your best today"	
+]
+
 
 function setGoal(){
-	document.getElementById("todaysGoal").innerHTML = goalContent;
+	document.getElementById("todaysGoal").innerHTML = goalsList[1];
 }
 
 function setQuote(){
-	document.getElementById("inspQuote").innerHTML = quoteContent;
+	document.getElementById("inspQuote").innerHTML = quotesList[0];
 }
 
 setGoal();
